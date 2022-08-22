bfd09778d0feb465424b4cf032c977549bafb860
Richie <richierichard439@gmail.com>
2022-08-22 17:18:14 +0100

    envoi test.c

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+int a;
+printf("donner un nombre:\n");
+scanf("%d",&a);
+if (a>0);
+{
+    printf("nombre est positf:\n");
+}
+else {
+    printf("incorect reessayer:\n");
+}
+    return 0;
+}
